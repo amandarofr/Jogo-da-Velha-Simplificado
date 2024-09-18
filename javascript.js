a8f007fddc5272cd96d899ffd6edeb6d90151264
Amanda Ferrucio <amanda.fr.mm@gmail.com>
2024-09-17 22:29:51 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,38 +1,48 @@
-//Começo , tabuleiro está vazio
-
-const linha = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
-        let jogadorAtual = 'X';
-
-function armazenarNaLinha(linha, posicao, letra) {
-            linha[posicao] = letra;
-        }
-//Meio , atualizar a celula com a letra X ou O no tabuleiro
-
-function atualizarCelula(indice, letra) {
-    const celula = document.querySelector(`.celula[data-index="${indice}"]`);
-            celula.textContent = letra;
-
-  document.querySelectorAll('.celula').forEach(celula => {
-            celula.addEventListener('click', function() {
-const indice = parseInt(this.getAttribute('data-index'));
-
-if (linha[indice] === ' ')
-  armazenarNaLinha(linha, indice, jogadorAtual);
-        atualizarCelula(indice, jogadorAtual);
-              
-jogadorAtual = jogadorAtual === 'X' ? 'O' : 'X';
-                }
-            });
-        });
-
-//Meio , vitoria definida pelo loop com as possiveis combinacoes
-  
+let jogador = "X";
+const combinacoesVitoria = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [2, 4, 6]  
+];
+//*Função de checar vitoria, através de um array feito pela classe .quadrado e validado através da variavel combinacoesVitoria
 function checarVitoria() {
-  const combinacoesVitoria=
-    [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-  for (const combinacao of combinacoesVitoria) {
-    const [a,b,c]=combinacao;
+    const cells = Array.from(document.querySelectorAll('.quadrado'));
+    for (const combinacao of combinacoesVitoria) {
+        const [a, b, c] = combinacao;
+        if (cells[a].innerHTML && cells[a].innerHTML === cells[b].innerHTML && cells[a].innerHTML === cells[c].innerHTML) {
+            return cells[a].innerHTML;
+        }
+    }
+    return null;
+}
+//*Função de mensagem quando há um ganhador
+function exibirMensagem(mensagem) {
+    const mensagemElement = document.getElementById('mensagem');
+    mensagemElement.textContent = mensagem;
+    mensagemElement.style.display = 'block';
+}
 
-//Fim - Empate
-  
+function jogar(celula) {
+    if (celula.innerHTML === "") {
+        celula.innerHTML = jogador;
+        const vencedor = checarVitoria();
+        if (vencedor) {
+            exibirMensagem(`Parabéns, jogador ${vencedor} ganhou!`);
+            return; 
+        }
+
+        
+        jogador = jogador === "X" ? "O" : "X";
+    }
+}
+function reiniciar() {
+    document.querySelectorAll('.quadrado').forEach(celula => {
+        celula.innerHTML = "";
+    });
+    jogador = "X";}
      
